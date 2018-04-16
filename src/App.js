96dabb9ab7364d5b15f6c17305009e206cde81ed
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-04-15 18:54:13 -0700

    functional app

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Jumbotron from "./components/Jumbotron";
 import characters from "./characters.json";
 import "./App.css";
 
-//
+let message = "";
+let topScore = 0;
+
 class App extends Component {
     state = {
-        characters
+        characters,
+        topScore,
+        message
     };
 
     score = 0; 
-
+  
     //handles operations when cards are clicked
     clickedCard = (id) => {
         const characterCard = this.state.characters.map(character => {
@@ -21,23 +25,30 @@ class App extends Component {
               if (character.clicked === false){  
                 character.clicked=true;
                 this.score++;
+                message = "You guessed Correctly!"
               } else {
-                alert("One too many clicks on the same fighter! Try again.");
+                message = "Oops! Try Again!";
                 this.resetGame(); 
               }
+              if(this.score > topScore){
+                  topScore = this.score;
+                this.setState({topScore});
+            
             } else {
             //when users score is 12 user wins game and game restarts
               if (this.score === 12) {
-                  alert("You win!");
+                  message = "You Win!";
                   this.resetGame();
               }
             }
+        }
             return characterCard;
           });
-        
+          this.setState({ topScore });
           this.setState({characters});
+          this.setState({message});
         };
-//function to shuffle each card in the array 
+//function to shuffle each card  
  shuffleCards = (array)=> {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -61,14 +72,22 @@ class App extends Component {
         const characterCard = this.state.characters.map(character => {
           character.clicked = false;
           return characterCard;
+          
         })
       }
       //renders jumbotron, score and images to the DOM, also calls the shuffleCard function when character is selected. 
         render() {
             return ( 
                 <div>
-                    <Wrapper>
-                        <Jumbotron> {this.score} </Jumbotron>
+                    <Jumbotron>
+                        {this.score}
+                        <h2 className="topScore">Top Score: {this.state.topScore}</h2>
+                        </Jumbotron>
+
+                        <h3>Click on image to earn points, but don't click on any more than once!</h3>
+                        <h1 className = "message">{this.state.message}</h1>
+                        
+                        <Wrapper>                      
                             {this.shuffleCards(this.state.characters).map(character => (
                             <CharacterCard
                                 id={character.id}
--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./Jumbotron";
+import "./Jumbotron.css"
 
 
 const Jumbotron = props => {
   return (
     <div className="main-container">
-      <div className="jumbotron">
-        <h1>Clicky Game</h1>
-        <p>Click on image to earn points, but don't click on any more than once!</p>
-          <h2 className = "Jumbotron">Your Score:{props.children}</h2>      
-    </div>
+      <div className="row">
+        <div className= "col-md-12">
+          <div className="jumbotron">
+            <h1>Clicky-Game</h1>
+               <h2 className = "Jumbotron">Your Score: {props.children}</h2>
+               <h2 className = "topScore">Top Score: {props.children}</h2>
+          </div>
+        </div>
+      </div>
   </div>
   )
 };
